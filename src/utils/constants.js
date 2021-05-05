803d89d113384fccdd5b8114776fc69e6ca2ea33
MajlonaA <majlona.aliaj@sjsu.edu>
2021-05-04 20:25:37 -0700

    new version

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -45,4 +45,5 @@ export const services = [
 export const products_url =
   'https://explorifyapi.netlify.app/.netlify/functions/PackageApi'
 
-export const single_product_url = `https://course-api.com/react-store-single-product?id=`
+export const single_product_url = `https://explorifyapi.netlify.app/.netlify/functions/products?id=
+`
